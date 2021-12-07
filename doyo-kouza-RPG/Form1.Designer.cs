2ef0db7d013fa5b8383aa6629ece246e8c7cde13
Ess2021 <87812053+Ess2021@users.noreply.github.com>
2021-12-07 10:54:46 +0900

    プロジェクト ファイルを追加します。

---
+++ doyo-kouza-RPG/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace doyo_kouza_RPG {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.EnsPic = new System.Windows.Forms.PictureBox();
+            this.EnsHP = new System.Windows.Forms.Label();
+            this.Console = new System.Windows.Forms.TextBox();
+            this.MyHP = new System.Windows.Forms.Label();
+            this.Sword = new System.Windows.Forms.Button();
+            this.Magic = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.EnsPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EnsPic
+            // 
+            this.EnsPic.Location = new System.Drawing.Point(13, 13);
+            this.EnsPic.Name = "EnsPic";
+            this.EnsPic.Size = new System.Drawing.Size(259, 148);
+            this.EnsPic.TabIndex = 0;
+            this.EnsPic.TabStop = false;
+            // 
+            // EnsHP
+            // 
+            this.EnsHP.AutoSize = true;
+            this.EnsHP.Location = new System.Drawing.Point(12, 193);
+            this.EnsHP.Name = "EnsHP";
+            this.EnsHP.Size = new System.Drawing.Size(38, 15);
+            this.EnsHP.TabIndex = 1;
+            this.EnsHP.Text = "label1";
+            // 
+            // Console
+            // 
+            this.Console.Location = new System.Drawing.Point(13, 167);
+            this.Console.Name = "Console";
+            this.Console.Size = new System.Drawing.Size(259, 23);
+            this.Console.TabIndex = 2;
+            // 
+            // MyHP
+            // 
+            this.MyHP.AutoSize = true;
+            this.MyHP.Location = new System.Drawing.Point(146, 193);
+            this.MyHP.Name = "MyHP";
+            this.MyHP.Size = new System.Drawing.Size(38, 15);
+            this.MyHP.TabIndex = 1;
+            this.MyHP.Text = "label1";
+            // 
+            // Sword
+            // 
+            this.Sword.Location = new System.Drawing.Point(13, 212);
+            this.Sword.Name = "Sword";
+            this.Sword.Size = new System.Drawing.Size(126, 37);
+            this.Sword.TabIndex = 3;
+            this.Sword.Text = "剣";
+            this.Sword.UseVisualStyleBackColor = true;
+            this.Sword.Click += new System.EventHandler(this.Sword_Click);
+            // 
+            // Magic
+            // 
+            this.Magic.Location = new System.Drawing.Point(146, 212);
+            this.Magic.Name = "Magic";
+            this.Magic.Size = new System.Drawing.Size(126, 37);
+            this.Magic.TabIndex = 3;
+            this.Magic.Text = "魔法";
+            this.Magic.UseVisualStyleBackColor = true;
+            this.Magic.Click += new System.EventHandler(this.Magic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Magic);
+            this.Controls.Add(this.Sword);
+            this.Controls.Add(this.Console);
+            this.Controls.Add(this.MyHP);
+            this.Controls.Add(this.EnsHP);
+            this.Controls.Add(this.EnsPic);
+            this.Name = "Form1";
+            this.Text = "土曜講座 - RPG";
+            ((System.ComponentModel.ISupportInitialize)(this.EnsPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox EnsPic;
+        private System.Windows.Forms.Label EnsHP;
+        private System.Windows.Forms.TextBox Console;
+        private System.Windows.Forms.Label MyHP;
+        private System.Windows.Forms.Button Sword;
+        private System.Windows.Forms.Button Magic;
+    }
+}
+
---
+++ doyo-kouza-RPG/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace doyo_kouza_RPG {
+    public partial class Form1 : Form {
+        //自分と敵のHPを宣言
+        private int 敵のHP;
+        private int 自分のHP;
+
+        public Form1() {
+            敵のHP = 200; //敵のHPの設定
+            自分のHP = 200; //自分のHPの設定
+            InitializeComponent();
+            EnsHP.Text = "敵のHP: " + 敵のHP.ToString(); //敵のHPを表示
+            MyHP.Text = "自分のHP: " + 自分のHP.ToString(); //自分のHPを表示
+        }
+
+        //aからbまでのランダム値を返す関数
+        private static int 乱数生成(int a, int b) {
+            Random p = new Random();
+            return p.Next(a, b + 1);
+        }
+
+        //攻撃用関数（ここを書いてください）
+        private void 攻撃(string 名前, int 強さ) {
+            Console.Text = 名前 + " によって敵は " + 強さ + " ダメージを受けた！"; //状況を表示
+            敵のHP = 敵のHP - 強さ; //敵のHPを減らす
+            EnsHP.Text = "敵のHP: " + 敵のHP.ToString(); //敵のHPを表示
+            判定(); //決着がついたか確認
+
+            Refresh(); //コンソールの再描画に必要
+            Thread.Sleep(2000); //2秒間停止
+
+            int 乱数 = 乱数生成(1, 3);
+            if (乱数 == 1) {
+                相手の攻撃("炎", 10);
+            } else if (乱数 == 2) {
+                相手の攻撃("拳", 20);
+            } else if (乱数 == 3) {
+                相手の攻撃("頭突き", 30);
+            }
+            判定(); //決着がついたか確認
+        }
+
+        //決着がついたか確認
+        private void 判定() {
+            if (敵のHP <= 0) {
+                MessageBox.Show("おめでとう、勝利した！");
+                this.Close();
+            }
+
+            if (自分のHP <= 0) {
+                MessageBox.Show("残念、負けてしまった。");
+                this.Close();
+            }
+        }
+
+        //相手の攻撃用関数
+        private void 相手の攻撃(string 名前, int 強さ) {
+            Console.Text = 名前 + " によって " + 強さ + " ダメージを受けてしまった！"; //状況を表示
+            自分のHP = 自分のHP - 強さ; //自分のHPを減らす
+            MyHP.Text = "自分のHP: " + 自分のHP.ToString(); //自分のHPを表示
+        }
+
+        private void Sword_Click(object sender, EventArgs e) {
+            攻撃("剣", 20);
+        }
+
+        private void Magic_Click(object sender, EventArgs e) {
+            攻撃("魔法", 50);
+        }
+    }
+}
